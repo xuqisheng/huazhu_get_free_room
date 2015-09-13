a27787ba0b940cdab586b666771eee0d069e8ed5
zhantong1994 <zhantong1994@163.com>
2015-09-13 23:07:41 +0800

    修改优化代码

--- huazhu.py
+++ huazhu.py
@@ -6,6 +6,20 @@ import time
 
 txtkey=re.compile(r'id="txtkey" value="(.*?)"')
 txtActivityType=re.compile(r'id="txtActivityType" value="(.*?)"')
+hdActivityCode=re.compile(r'id="hdActivityCode" value="(.*?)"')
+
+the_code={
+	'99':'太棒了！恭喜！您已成功兑换免房券,快去您的账户查看吧！',
+	'2.1':'不在开抢日期！',
+	'3':'不要着急哦，马上登场！',
+	'4':'IP错误',
+	'12':'系统错误',
+	'97':'啊呀，手慢了...免房兑换已满',
+	'5':'每个会员至多抢一张免房哟！把机会留给其他人吧！',
+	'103':'啊呀，手慢了...免房兑换已满',
+	'104':'啊呀，手慢了...免房兑换已满'
+}
+
 headers={
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/web',
 	'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
@@ -15,6 +29,8 @@ def init():
 	global opener
 	global sign
 	global ActivityType
+	global activityCode
+	global memberID
 	cj=http.cookiejar.CookieJar()
 	opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 
@@ -24,6 +40,13 @@ def init():
 	content=get.read().decode('utf-8')
 	sign=txtkey.findall(content)[0]
 	ActivityType=txtActivityType.findall(content)[0]
+	activityCode=hdActivityCode.findall(content)[0]
+	print(activityCode)
+	js_url=re.findall(r'"(http://ws-www.hantinghotels.com/Content_activty/js/SecondKill\.js.*?)"',content)[0]
+	js=urllib.request.urlopen(js_url)
+	js_con=js.read().decode()
+	memberID=re.findall(r"memberID: '(.*?)'",js_con)[0]
+	print(memberID)
 
 def isanom():
 	url_isanom='http://activity.h-world.com/PointExchangeRoom/IsAnom'
@@ -86,22 +109,30 @@ def test():
 	print(result)
 
 def get_room():
+	hotel=['汉庭酒店苏州新观前店','汉庭酒店苏州大学葑门店','汉庭酒店苏州园区金鸡湖店']
 	while True:
 		url='http://activity.h-world.com/PointExchangeRoom/Exchange'
 		data={
-		'activityCode':'skill09',
+		'activityCode':activityCode,
 		'storeName':'汉庭酒店苏州新观前店',
-		'memberID':'031006407',
+		'memberID':memberID,
 		'strDate':'2015/9/14'
 		}
 		post=urllib.request.Request(url=url,headers=headers,data=urllib.request.urlencode(data).encode(),method='POST')
 		exchange=opener.open(post)
 		result=json.loads(exchange.read().decode())
-		print(result['code'])
+		code=result['code']
+		try:
+			sign=the_code[code]
+			print(sign)
+		except Exception:
+			print(result)
+		if sign=='5':
+			data
 		time.sleep(5)
 
 if __name__=='__main__':
 	init()
-	send_message()
-	test()
-	get_room()
+#	send_message()
+#	test()
+#	get_room()
